1510e76f88edf3c7fc4a38a27072d454f3f40a6c
Chindarat <buytaxusa@gmail.com>
2026-08-19 22:16:18 +0700

    feat: initialize Django project structure with basic templates, CSS, and routing configurations

--- web/views.py
+++ web/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    import datetime
+
+    context = {
+        "title": "My Home Page",
+    }
+
+    context["date"] = datetime.date.today()
+    return render(request, "index.html", context)
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
