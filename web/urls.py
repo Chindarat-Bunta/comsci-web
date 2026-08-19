61647022f9bc56ea56331040536599ed2b0939a1
wanida <wnidasrikaew@gmail.com>
2026-08-19 17:44:16 +0700

    feat: add curriculum, faculty, and contact pages with corresponding routes and views

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("curriculum", views.curriculum, name="curriculum"),
+    path("faculty", views.faculty, name="faculty"),
+    path("contact", views.contact, name="contact"),
 ]
